520ead9c83b47ca26cf4208ed620a7e7f0a4f458
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2019-02-18 23:34:55 +0330

    Add files via upload

---
+++ smlthegreat.js
@@ -0,0 +1,28 @@
+//in the name of God
+//mahdisml (mahdi safarmohammadloo)
+document.body.style.backgroundColor = "white";
+unfade(document.body);
+function fade(element) {
+    var op = 1;
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+function unfade(element) {
+    var op = 0.005;
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.005;
+    }, 10);
+}
